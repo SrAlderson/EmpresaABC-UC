9d388a8f1668d0b2203f098914d92c53e6489c53
SrAlderson <srgonzalez1997@hotmail.com>
2023-11-08 20:45:49 -0500

    Modificacion | Clase Usuario parametros para la BD

--- src/main/java/com/empresa/demo/model/usuario.java
+++ src/main/java/com/empresa/demo/model/usuario.java
@@ -15,78 +15,78 @@ public class usuario {
     //Atributos del usuario
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Esto con el fin de poder realizar de manera automatica la asignacion de un ID
-    private int id; 
-    private String primerNombre; 
-    private String segundoNombre;
-    private String primerApellido;
-    private String segundoApellido;
-    private String direccion;
-    private String correo;
-    private int cedula;
+    private int usu_id; 
+    private String usu_primerNombre; 
+    private String usu_segundoNombre;
+    private String usu_primerApellido;
+    private String usu_segundoApellido;
+    private String usu_direccion;
+    private String usu_correo;
+    private int usu_cedula;
 
     //Generacion del Metodo constructor
 
-    public usuario(String primerNombre, String segundoNombre,  String primerApellido, String segundoApellido,  String direccion, String correo, int cedula){
+    public usuario(String usu_primerNombre, String usu_segundoNombre,  String usu_primerApellido, String usu_segundoApellido,  String usu_direccion, String usu_correo, int usu_cedula){
         super();
-        this.primerNombre = primerNombre;
-        this.segundoNombre = segundoNombre;
-        this.primerApellido = primerApellido;
-        this.segundoApellido = segundoApellido;
-        this.direccion = direccion;
-        this.correo = correo;
-        this.cedula = cedula;
+        this.usu_primerNombre = usu_primerNombre;
+        this.usu_segundoNombre = usu_segundoNombre;
+        this.usu_primerApellido = usu_primerApellido;
+        this.usu_segundoApellido = usu_segundoApellido;
+        this.usu_direccion = usu_direccion;
+        this.usu_correo = usu_correo;
+        this.usu_cedula = usu_cedula;
     }
 
     //Generacion de Get
     public int getId(){
-        return id;
+        return usu_id;
     }
     public String getNombre(){
-        return primerNombre;
+        return usu_primerNombre;
     }
     public String getNombre2(){
-        return segundoNombre;
+        return usu_segundoNombre;
     }
     public String getApellido(){
-        return primerApellido;
+        return usu_primerApellido;
     }
     public String getApellido2(){
-        return segundoApellido;
+        return usu_segundoApellido;
     }
     public String getDireccion(){
-        return direccion;
+        return usu_direccion;
     }
     public String getCorreo(){
-        return correo;
+        return usu_correo;
     }
     public int getCedula(){
-        return cedula;
+        return usu_cedula;
     }
 
     //Generacion de Set
-    public void setId(int id){
-        this.id = id;
+    public void setId(int usu_id){
+        this.usu_id = usu_id;
     }
-    public void setNombre(String primerNombre){
-        this.primerNombre = primerNombre; 
+    public void setNombre(String usu_primerNombre){
+        this.usu_primerNombre = usu_primerNombre; 
     }
-    public void setNombre2(String segundoNombre){
-        this.segundoNombre = segundoNombre;
+    public void setNombre2(String usu_segundoNombre){
+        this.usu_segundoNombre = usu_segundoNombre;
     }
-    public void setApellido(String primerApellido){
-        this.primerApellido = primerApellido;
+    public void setApellido(String usu_primerApellido){
+        this.usu_primerApellido = usu_primerApellido;
     }
-    public void setApellido2 (String segundoApellido){
-        this.segundoApellido = segundoApellido;
+    public void setApellido2 (String usu_segundoApellido){
+        this.usu_segundoApellido = usu_segundoApellido;
     }
-    public void setDireccion (String direcccion){
-        this.direccion = direcccion;
+    public void setDireccion (String usu_direcccion){
+        this.usu_direccion = usu_direcccion;
     }
-    public void setCorreo(String correo){
-        this.correo = correo;
+    public void setCorreo(String usu_correo){
+        this.usu_correo = usu_correo;
     }
-    public void setCedula (int cedula){
-        this.cedula = cedula;
+    public void setCedula (int usu_cedula){
+        this.usu_cedula = usu_cedula;
     }
 
     // Creacion de la conexion con la base de datos
